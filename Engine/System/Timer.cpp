778534c44941dfb8aa558a1933c8d33489c01e7e
wewe231223@Desktop <wewe231223@kpu.ac.kr>
2024-01-31 00:11:58 +0900

    Add Timer

--- Engine/System/Timer.cpp
+++ Engine/System/Timer.cpp
@@ -1,2 +1,72 @@
 #include "EnginePch.h"
 #include "Timer.h"
+namespace System {
+	Timer::Timer() {
+		INT64 CountsPerSecond{};
+		::QueryPerformanceFrequency((LARGE_INTEGER*)&CountsPerSecond);
+		m_fSecondsPerCount = 1.f / static_cast<float>(CountsPerSecond);
+	}
+
+	float Timer::GetTimeElapsed() const {
+		if (m_bStopped) {
+			// 멈춰있는 경우 지나간 시간은 제외한다 
+			return static_cast<float>(((m_nStopedTime - m_nPausedTime) - m_nBaseTime)) * m_fSecondsPerCount;
+		}
+		else {
+			// 그렇지 않다면, 현재 시간을 기준으로 계산 
+			return static_cast<float>(((m_nCurrentTime - m_nPausedTime) - m_nBaseTime)) * m_fSecondsPerCount;
+		}
+	}
+
+	float Timer::GetDeltaTime() const {
+		return m_fDeltaTime;
+	}
+
+	void Timer::Reset() {
+		INT64 CurrentTime{};
+		::QueryPerformanceCounter((LARGE_INTEGER*)&CurrentTime);
+
+		m_nBaseTime = CurrentTime;
+		m_nPrevTime = CurrentTime;
+		m_nStopedTime = 0;
+		m_bStopped = false;
+	}
+
+	void Timer::Start() {
+		INT64 StartTime{};
+		::QueryPerformanceCounter((LARGE_INTEGER*)&StartTime);
+
+		if (m_bStopped) {
+			m_nPausedTime += (StartTime - m_nStopedTime);
+			m_nPrevTime = StartTime;
+			m_nStopedTime = 0;
+			m_bStopped = false;
+		}
+	}
+
+	void Timer::Stop() {
+		if (!m_bStopped) {
+			INT64 CurrentTime{};
+			::QueryPerformanceCounter((LARGE_INTEGER*)&CurrentTime);
+			m_nStopedTime = CurrentTime;
+			m_bStopped = true;
+		}
+	}
+
+	void Timer::Update() {
+		if (m_bStopped) {
+			m_fDeltaTime = 0.f;
+			return;
+		}
+
+		INT64 CurrentTime{};
+		::QueryPerformanceCounter((LARGE_INTEGER*)&CurrentTime);
+		m_nCurrentTime = CurrentTime;
+		m_fDeltaTime = (m_nCurrentTime - m_nPrevTime) * m_fSecondsPerCount;
+		m_nPrevTime = CurrentTime;
+
+		if (m_fDeltaTime < 0.f) {
+			m_fDeltaTime = 0.f;
+		}
+	}
+}
---
+++ Engine/System/Timer.h
@@ -0,0 +1,31 @@
+#pragma once
+namespace System {
+	class Timer {
+	public:
+		Timer();
+	private:
+		float m_fSecondsPerCount{};
+		float m_fDeltaTime{ -1.f };
+
+		INT64 m_nBaseTime{};
+		INT64 m_nPausedTime{};
+		INT64 m_nStopedTime{};
+		INT64 m_nPrevTime{};
+		INT64 m_nCurrentTime{};
+
+		bool m_bStopped{ false };
+
+		float m_fFpsTimeElapsed{ 0.f };
+		int m_nFrameCount{ 0 };
+
+	public:
+		float GetTimeElapsed() const;
+		float GetDeltaTime() const;
+
+		void Reset();
+		void Start();
+		void Stop();
+		void Update();
+	};
+
+}
